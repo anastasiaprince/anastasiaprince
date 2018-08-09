402350311d1f3a0ce7e019fb3137f34fc2a668a1
Anastasia Prince <msanaprince@gmail.com>
2018-08-09 07:45:14 +0100

    Add project descriptions

--- app/js/initialize.js
+++ app/js/initialize.js
@@ -1,5 +1,9 @@
 import someFunction from './someFile';
 
+const $ = require('jquery');
+
+console.log('Tasty Brunch, just trying to use jQuery!', $('body'));
+
 document.addEventListener('DOMContentLoaded', () => {
   console.log('initialized');
   someFunction();
